087a151df27e9cf8a01dd2ecdf815a4e20cb06cc
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-10-08 13:48:22 +0300

    Replacement server.js file for the project.

--- src/server.js
+++ src/server.js
@@ -1,30 +1,36 @@
-const httpProxy = require('http-proxy')
+const http = require('http')
+const httpProxy = require('http-proxy-middleware')
 const data = require('./data/netData-temp.js')
-
+const ipRangeCheck = require('ip-range-check')
 
 for (const netData of data) {
-  const Fastify = require('fastify')()
-  const proxyServer = httpProxy.createProxyServer({})
-
-  Fastify.all(netData.node_url, (req, reply) => {
-    const sourceIP = req.ip
-    const allowedIPs = [netData.client_net]
-    if (allowedIPs.some(network => req.isInCIDR(sourceIP, network))) {
-      proxyServer.web(req, reply.res, {
-        target: netData.target
-      })
-    } else {
-      reply.code(403).send('Access denied')
+  const proxy = httpProxy.createProxyMiddleware({
+    target: netData.target,
+    changeOrigin: true,
+    xfwd: true,
+    router: {
+      [netData.node_url]: netData.target
+    },
+    onProxyReq: (proxyReq, req, res) => {
+      const sourceIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+      const allowedIPs = [netData.client_net]
+      console.log('Source IP: ' + sourceIP)
+      console.log('Allowed IPs: ' + allowedIPs)
+      if (!allowedIPs.some(network => ipRangeCheck(sourceIP, network))) {
+        res.writeHead(403, { 'Content-Type': 'text/plain' })
+        res.end('Access denied for ' + sourceIP + '\n')
+      }
     }
   })
 
-  Fastify.listen({ port: 8181, host: netData.server_node }, (err, address) => {
-    if (err) {
-      console.error(err)
-      process.exit(1)
-    }
-    console.log(`Proxy server for VLAN_${netData.vlan_number} listening on ${address}`)
+  const server = http.createServer((req, res) => {
+    proxy(req, res, (err) => {
+      res.writeHead(500, { 'Content-Type': 'text/plain' })
+      res.end('Something went wrong.')
+    })
+  })
+
+  server.listen(netData.port, netData.server_node, () => {
+    console.log(`Proxy server for VLAN_${netData.vlan_number} listening on http://${netData.server_node}:${netData.port}${netData.node_url}`)
   })
 }
-
-
