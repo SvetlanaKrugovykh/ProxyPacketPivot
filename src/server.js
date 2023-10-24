a0572d38a4550b64f7d5c2aa14f656bbfbb26f0f
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-10-24 21:24:22 +0300

    added timestamp and tcp to dns

--- src/server.js
+++ src/server.js
@@ -1,10 +1,11 @@
-const https = require('https')
-const httpProxy = require('http-proxy')
-require('dotenv').config()
-const dns2 = require('dns2')
+const https = require(`https`)
+const httpProxy = require(`http-proxy`)
+require(`dotenv`).config()
+const dns2 = require(`dns2`)
 const Packet = dns2.Packet
-const { data, cert } = require('./data/netData.js')
+const { data, cert } = require(`./data/netData.js`)
 const DEBUG_LEVEL = Number(process.env.DEBUG_LEVEL) || 0
+const currentTime = new Date().toLocaleString('en-US', { dateStyle: `medium`, timeStyle: `short` });
 
 //#region DNS
 for (const netData of data) {
@@ -24,11 +25,11 @@ for (const netData of data) {
         address: netData.proxyIP,
       })
       send(response)
-      if (DEBUG_LEVEL === 7) console.log(`Response: ${JSON.stringify(response)}`)
+      if (DEBUG_LEVEL === 7) console.log(`Response at ${currentTime}: ${JSON.stringify(response)}`)
     }
   })
   server.on('request', (request, response, rinfo) => {
-    if (DEBUG_LEVEL === 7) console.log(`Request: ${JSON.stringify(request)}`)
+    if (DEBUG_LEVEL === 7) console.log(`Request at ${currentTime}: ${JSON.stringify(request)}`)
   })
 
   server.on('requestError', (error) => {
@@ -40,7 +41,7 @@ for (const netData of data) {
   })
 
   server.on('close', () => {
-    console.log('server closed')
+    console.log(`server closed at ${currentTime}`)
   })
 
   server.listen({
@@ -48,6 +49,11 @@ for (const netData of data) {
       port: netData.dnsPort,
       address: netData.server_node,
       type: "udp4",
+    },
+    tcp: {
+      port: netData.dnsPort,
+      address: netData.server_node,
+      type: "tcp4",
     },
   })
 }
@@ -77,34 +83,34 @@ for (const netData of data) {
   //#region errorLog
   if (DEBUG_LEVEL > 0) {
     server.on('request', (req, res) => {
-      console.log('Request:', req.url)
+      console.log(`Request at ${currentTime}:`, req.url)
     })
     server.on('error', (err) => {
-      console.error('Server error:', err)
+      console.error(`Server error at ${currentTime}:`, err)
     })
     proxy.on('error', function (err, req, res) {
-      console.error('Proxy error:', err);
-      if (err.code === 'UNABLE_TO_VERIFY_LEAF_SIGNATURE') {
-        console.error('The root CA certificate might not be trusted.');
+      console.error(`Proxy error at ${currentTime}:`, err);
+      if (err.code === `UNABLE_TO_VERIFY_LEAF_SIGNATURE`) {
+        console.error(`The root CA certificate might not be trusted.`);
       }
     })
   }
   //#endregion errorLog
   proxy.on('proxyReq', function (proxyReq, req, res, options) {
     req.url = netData.target //cert.name
-    console.log('Proxy request:', req.url)
+    console.log(`Proxy request at ${currentTime}:`, req.url)
   })
 
   //#region detailedLog
   if (DEBUG_LEVEL > 3) {
     proxy.on('proxyRes', function (proxyRes, req, res) {
-      console.log('Proxy response:', req.url)
+      console.log(`Proxy response at ${currentTime}:`, req.url)
     })
   }
   //#endregion detailedLog
 
   server.listen(netData.port, netData.server_node, () => {
-    console.log(`Proxy server listening on https://${netData.server_node}:${netData.port}`)
+    console.log(`Proxy server listening at ${currentTime} on https://${netData.server_node}:${netData.port}`)
   })
 }
 //#endregion HTTP/HTTPS
